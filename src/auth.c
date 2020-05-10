568a7ea32ce6c1686a60be399c29ee0141342660
jwilme <wilme.jacques@gmail.com>
2020-05-10 15:43:08 +0200

    WIP : Implementing the DB and Master PSK auth
    Those functions, plus a function that prompt for a password on the
    standard input, init the connection with the database, and generate
    from a given password the master Pre-Shared-Key. This master PSK is used
    to decypher the other stored Pre-Shared-Keys.

--- src/auth.c
+++ src/auth.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <mosquitto.h>
 #include <mosquitto_broker.h>
@@ -9,40 +10,84 @@
 #include "mysql.h"
 #include "crypto.h"
 
-#define LOG_AUTH_ERROR 		"[AUTH - ERROR] ::"
-#define LOG_AUTH_WARNING 	"[AUTH - WARNING] ::"
-#define LOG_AUTH_INFO 		"[AUTH - INFO] ::"
+#define LOG_AUTH_ERROR_PREFIX 		"[AUTH - ERROR] ::"
+#define LOG_AUTH_WARNING_PREFIX 	"[AUTH - WARNING] ::"
+#define LOG_AUTH_INFO_PREFIX 		"[AUTH - INFO] ::"
 
 #define auth_log_error(char * fmt, ...) \
-	plugin_log(MOSQ_LOG_WARNING, LOG_AUTH_ERROR, char *fmt, ...)
+	plugin_log(MOSQ_LOG_WARNING, LOG_AUTH_ERROR_PREFIX, char *fmt, ...)
 
 #define auth_log_warning(char * fmt, ...) \
-	plugin_log(MOSQ_LOG_ERROR, LOG_AUTH_WARNING, char *fmt, ...)
+	plugin_log(MOSQ_LOG_ERROR, LOG_AUTH_WARNING_PREFIX, char *fmt, ...)
 
 #define auth_log_info(char * fmt, ...) \
-	plugin_log(MOSQ_LOG_INFO, LOG_AUTH_INFO, char *fmt, ...)
+	plugin_log(MOSQ_LOG_INFO, LOG_AUTH_INFO_PREFIX, char *fmt, ...)
 
-char * psk_key;
-
-int psk_master_auth(const char * password, char * out_key)
+int db_connection(void *user_data, const char *username, 
+		const char *table_name, const char* db_path)
 {
-	char * username = MASTER_PSK_USERNAME;
+	/* Prompt for Password and Attempt to Connect to the DB */
+	for(int i = 0; i <= RETRY_LIMITS; i++){
+		if(i == RETRY_LIMITS){
+			auth_log_error("Max number of MySQL Authentication "
+					"attempts reached");
+			return AUTH_DENIED;
+		}
 
-	switch(unpwd_client_auth( (const char *) username, password)){
+		char * db_password;
+		if(prompt_password(&db_password) < 0){
+			auth_log_error("Unable to prompt for the password");	
+			continue
+		} 
+		
+		else if(mysql_connect(user_data, username) == DB_SUCCESS){
+			break;
+		}
+	       
+		else{
+			free(db_password);	
+		}
+	}
 
-	case(AUTH_SUCCESS):
-		if(compute_master_key(password, out_key))
-			return AUTH_FAILURE;
+	free(db_password);
+	return AUTH_SUCCESS;
+}
 
-		return AUTH_SUCCESS; 
+int psk_init(char * psk_generated_key)
+{
+	/* Prompt for Password and Check that it is the correct key */
+	for(int i = 0; i <= RETRY_LIMITS; i++){
+		if(i == RETRY_LIMITS){
+			auth_log_error("Max number of PSK Authentication "
+					"attempts reached");
+			return AUTH_DENIED;
+		}
 
-	case(AUTH_DENIED):
-		return AUTH_DENIED;
+		char * psk_password;
+		if(prompt_password(&psk_password) < 0){
+			auth_log_error("Unable to prompt for the password");	
+			continue;
+		}
 
-	case(AUTH_FAILURE):
+		if(unpwd_client_auth(MASTER_PSK_USERNAME, psk_password)) break;
+		else free(psk_password);
+	}
+
+	/* Generate and store the PSK Master Key */
+	char salt[SALT_LEN + 1]; 
+	fetch_salt(MASTER_PSK_USERNAME, psk_salt);
+
+	int err = compute_master_key(psk_password, salt, out_key);
+
+	free(psk_password);	
+
+	if(err){
+		auth_log_error("Could not generate the PSK Master Key");
 		return AUTH_FAILURE;
-
-	}	
+	} else {
+		auth_log_info("Successfuly generated the PSK Master Key");
+		return AUTH_SUCCESS;
+	}
 }
 
 int unpwd_client_auth(const char * username, const char * password)
@@ -93,6 +138,22 @@ int psk_client_auth(const char * identity, char * psk_key)
 	return decypher_error ? AUTH_FAILURE: AUTH_SUCCESS;	
 }
 
-int get_pass(const char * , char *){
+int prompt_password(char ** password)
+{	
+	struct termios old, new;
+	int nread;
 
+	if(tcgetattr(stdin, &old) != 0)
+		return -1;
+
+	new = old;
+	new.c_lflag &= ~ECHO;
+	if(tcsetattr(stdin, TCSAFLUSH, &new) != 0)
+		return -1;
+
+	nread = getline(lineptr, MAX_PASSWORD_SIZE, stdin);
+
+	(void) tcsetattr(stdin, TCSAFLUSH, &old);
+
+	return nread;
 }
--- src/auth.h
+++ src/auth.h
@@ -7,7 +7,10 @@ enum Auth_ErrorCodes {
 }
 
 #define MASTER_PSK_USERNAME "master_psk"
+#define MAX_PASSWORD_SIZE 40
+#define RETRY_LIMITS 3
 
+int db_connection(const char *user, const char *password);
 int psk_master_auth(const char *password, char *out_key);
 int unpwd_client_auth(const char *username, const char *password);
 int psk_client_auth(const char *identity, char *psk_key);
