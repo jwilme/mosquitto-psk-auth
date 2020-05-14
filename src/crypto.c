e5c77cc23fa7f353ed53dbf547f6712c6821594d
jwilme <wilme.jacques@gmail.com>
2020-05-14 15:03:44 +0200

    Corrected some syntax error and updated comments
    The comments are not at their approriate place atm, I'll move them to
    the header file when the coding of the functions will be ready.

--- src/crypto.c
+++ src/crypto.c
@@ -32,7 +32,9 @@
  * Function: hash_password
  *
  * This function takes a password, concatenate the salt at the end of it, and
- * compute the Argon2 Hash 
+ * compute the Argon2 Hash. The point of generating such a hash is to check
+ * if the couple username/password given by the client matches the one 
+ * stored in the databse, without ever storing the password in plaintext.
  *
  * Parameters:
  *	password:	Pointer to the C-String containing the password that 
@@ -67,6 +69,30 @@ int hash_password(const char * password, const char * salt, char * hash){
 }
 
 /*
+ * Function: compute_master_key
+ *
+ * This functions takes a password and a salt, concatenate both and hash the
+ * resulting string using SHA256. The point is to generate with a user-typed
+ * password and a randomly generated salt a 256 bits keys that is going to be
+ * used to uncypher stored PSK.  
+ *
+ * Parameters: 
+ * 	password:	A pointer to a C-String containing the user-typed 
+ * 			password
+ * 	salt:		A pointer to a C-String containing the random generated
+ * 			salt
+ * 	out:		A pointer to an allocated buffer which will contain the
+ * 			resulting hash (the psk master key)
+ *
+ * Return Value: 
+ * 	CRYPTO_OK if the hash could be genereated properly
+ * 	CRYPTO_FAILURE if not (e.g. a malloc failed)
+ *
+ * TODO:
+ * 	For the moment, this functions only support the SHA256 algorithm. In
+ * 	the future, it could be interesting to support type of hash such as 
+ * 	MD5 (booo) or SHA3, SHA512 depending on the wanted length of the 
+ * 	key.
  */
 int compute_master_key(const char *password, 
 		const char *salt, char *out)
@@ -90,6 +116,27 @@ int compute_master_key(const char *password,
 	return CRYPTO_OK;
 }
 
+/*
+ * Function: decypher_key
+ *
+ * This function takes a cyphered key and its init vector and uncyphers 
+ * it using the AES256_CBC algorithm
+ *
+ * Parameters:
+ *	key: 		The Master-Key used to cypher/decypher the cyphered key
+ *	cypher: 	The Cyphered Key
+ *	iv: 		The Init Vector used to cypher the Key
+ *	out_key: 	The Uncyphered KEY
+ *
+ * Return Value:
+ *	CRYPTO_OK if the psk has been successfully decyphered 
+ *	CRYPTO_FAILURE otherwise 
+ *
+ * TODO:
+ * 	At the moment, the only algorithm supported is AES256_CBC, it could be 
+ * 	interesting in the future to integrate other cyphers such as Blowfish or
+ * 	Triple DES for example. 	
+ */
 int decypher_key(const char * key, const char * cypher, 
 		const char * iv, char * out_key)
 {
@@ -97,38 +144,43 @@ int decypher_key(const char * key, const char * cypher,
 	int total_len, len;
 	int err; 
 
-	//XXX : wtf is this? Need to read the man page or the header
-	if(!(ctx = EVP_CIPHER_CTX_new()))
+	if(!(ctx = EVP_CIPHER_CTX_new())){
+		crypto_log_error("Could not create a new cypher context");
 		return CRYPTO_FAILURE;	
+	}
 
 	err = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, 
 				(unsigned char *)key, (unsigned char *)iv);
 	if(err){
-		//TODO : Log
-		//TODO : Jump to clean 
+		crypto_log_error("Could not Initiate the Decypher");
+		goto decypher_cleanup;
 	}	
 
-	err = (!EVP_DecryptUpdate(ctx, out_key, &len, cypher, KEY_LEN));
+	err = (!EVP_DecryptUpdate(ctx, (unsigned char *)out_key, &len, 
+				(unsigned char *)cypher, strlen(cypher)));
 	if(err){
-		//TODO : Log
-		//TODO : Jump to clean 
+		crypto_log_error("Could not complete the update step of the"
+				 "AES decyphering"); 
+		goto decypher_cleanup;
 	}	
 
 	total_len = len;
 
-	err = EVP_DecryptFinal_ex(ctx, out_key + len, &len);
+	err = EVP_DecryptFinal_ex(ctx, (unsigned char *)(out_key + len), &len);
 	if(err){
-		//TODO : Log
-		//TODO : Jump to clean 
+		crypto_log_error("Could not complete the Final step of AES" 
+				 "decyphering");
+		goto decypher_cleanup;
 	}	
 
 	total_len += len;
 	if(total_len != KEY_LEN){
-		//TODO Set err
-		//TODO Log
+		err = 1;
+		crypto_log_error("The generated key is not the right size");
 	}
 
+decypher_cleanup:
+
 	EVP_CIPHER_CTX_free(ctx);
-	
 	return err ? CRYPTO_FAILURE : CRYPTO_OK;
 }
