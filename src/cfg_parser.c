13449a4041ae0d7dd6618cefc37522d7e01deb65
jwilme <wilme.jacques@gmail.com>
2020-05-24 15:15:32 +0200

    Added documentation to the cfg_parser functions
    I also commented a bit the code of the long functions, so I'm not
    lost when I'll come back to this project

--- src/cfg_parser.c
+++ src/cfg_parser.c
@@ -11,6 +11,34 @@
 #include "plugin.h"
 #include "cfg_parser.h"
 
+/*
+ * Function: _set_parameter
+ *
+ * This function takes a setting from the configuration file, and sets the 
+ * corresponding field of the settings structure of the entity currently being
+ * configured to the value contained by the setting of the configuration file.
+ *
+ * Parameters:
+ * 	field: 		The setting from the configuration file 
+ * 	type:		The type of the value that the setting contains
+ * 	cnt:		The number of settings of the entity that is being
+ * 			configured and that matches the type of the value
+ * 			contained in the <field> argument
+ * 	settings:	An array of string representing the possible 
+ * 			settings of the entity being configured that matches
+ * 			the type of the value of <field> 
+ * 	set_map:	The settings_layout structure that describes the
+ * 			layout of the settings structure of the entity
+ * 			that is being configured 
+ *
+ * Return Value:
+ * 	CFG_SUCCESS 	if the given settings does exist, and the type of the
+ * 			setting corresponds to the actual setting
+ * 	CFG_FAILURE 	if an error occured, usually if the given setting 
+ * 			does not exist, or the configuration type mismatches
+ * 			the actual one
+ *
+ */ 
 int _set_parameter(config_setting_t *field, int type,
 		int cnt, const char **settings, 
 		const struct Settings_layout *set_map)
@@ -41,19 +69,40 @@ int _set_parameter(config_setting_t *field, int type,
 	}
 
 	plugin_log_error("<%s:%d-%s> : This option does not" 
-			"exist for the given back-end.",
+			"exist.",
 			config_setting_source_file(field),
 			config_setting_source_line(field),
 			name);
 	return CFG_ERROR;
 }
 
+/*
+ * Function: read_paramaters
+ *
+ * This function configures an entity of the plugin accordingly to the values
+ * that are given in a specific configuration group of the config file.
+ *
+ * Parameters:
+ * 	cfg:		A config_t structure that resulted from the reading of
+ * 			the config file
+ * 	setting_name:	A string containing the name of the configuration group
+ * 			that contains the settings needed to configure the 
+ * 			entity
+ * 	set_map:	A Setting_layout structure describing the setting
+ * 			structure of the entity that is being configured 	
+ *
+ * Return Value :
+ * 	CFG_SUCCESS 	if the entity has been properly configured
+ * 	CFG_ERROR	if an error occured e.g. an error in the config file or
+ * 			an error while configure the entity
+ */
 int read_parameters(config_t *cfg, const char *setting_name,
 		const struct Settings_layout *set_map)
 {
 	int count;
 	config_setting_t * setting = config_lookup(cfg, setting_name);
 
+	/* Fetch the number of settings in the setting group */
 	if(setting)
 		count = config_setting_length(setting);
 
@@ -63,6 +112,7 @@ int read_parameters(config_t *cfg, const char *setting_name,
 		return CFG_SUCCESS;
 	}
 
+	/* Treat each settings of the setting group */
 	for(int i = 0; i < count; i++){
 		config_setting_t *field = config_setting_get_elem(setting, i);
 		int type = config_setting_type(field); 
@@ -98,6 +148,8 @@ int read_parameters(config_t *cfg, const char *setting_name,
 
 			return CFG_ERROR;	
 		}
+		/* Check that the setting exists, and set it to the value 
+		 * given in the config file */
 		if(_set_parameter(field, type, cnt, settings, set_map))
 			return CFG_ERROR;
 	}
@@ -107,6 +159,7 @@ int read_parameters(config_t *cfg, const char *setting_name,
 int configure_plugin(const char *filename, struct DB_instance *db_i){
 	const char *str;
 
+	/* Open and read the configuration file */
 	config_t struct_cfg;
 	config_t *cfg = &struct_cfg;
 	config_init(cfg);
@@ -121,6 +174,8 @@ int configure_plugin(const char *filename, struct DB_instance *db_i){
 		return CFG_ERROR;	
 	}
 
+	/* Search for the back_end settings, and check that it corresponds to
+	 * an implemented back-end */
 	if(config_lookup_string(cfg, "back_end", &str)){
 		if(!strcmp(str, "mysql")){
 			mysql_cfg_init(db_i);
--- src/cfg_parser.h
+++ src/cfg_parser.h
@@ -8,4 +8,21 @@ enum CFG_ReturnCode{
 	CFG_ERROR = 1
 };
 
+/*
+ * Function: configure_plugin
+ *
+ * This functions opens and reads a configuration file and set the DB_instance 
+ * and a DB_setting structure consequently.
+ *
+ * Parameters:
+ * 	filename:	A string containing an absolute or relative file to an 
+ * 			auth plugin configuration file
+ * 	db_i:		A pointer to an allocated DB_instance structure that 
+ * 			needs to be initialized.
+ *
+ * Returns:
+ * 	CFG_SUCCESS if both the structures have been properly initialized
+ * 	CFG_ERROR if the configuration could not be opened contained errors.
+ * 
+ */
 int configure_plugin(const char *filename, struct DB_instance *db_i);
