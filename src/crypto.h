5d3c129111bfcc054dd2d90b1aabe70dc0fffcbe
jwilme <wilme.jacques@gmail.com>
2020-05-25 10:31:39 +0200

    Updated the commentary of the cryptographic functions

--- src/crypto.h
+++ src/crypto.h
@@ -5,33 +5,6 @@ enum Crypto_ErrorCodes {
 	CRYPTO_FAILURE = 1
 };
 
-/* Hashing Fuctions for Password Checking :
- *	Implemented : Argon2, 
- *	To be implemented (MAYBE) : PBKDF2, Bcrypt, Scrypt
- *
- * Hashing Functions for Key Generation :
- * 	Implemented : SHA256 + SALT,
- *	To be implemented (MAYBE) : SHA1, MD5
- *
- * Symmetric Cypher for PSK decyphering :
- * 	Implemented : AES256-CBC
- * 	To be implemented (for sure) : AES128-CBC
- * 	To be implemented (MAYBE) : Triple DES, Blowfish
- *
- *
- */
-enum Crypto_PwHash {
-	ARGON2 = 1
-};
-
-enum Crypto_KeyHash {
-	SHA_256= 1
-};
-
-enum Crpyto_SymCyp {
-	AES256_CBC = 1
-};
-
 #define KEY_LEN 32
 #define HASH_LEN 32
 #define SALT_LEN 32
@@ -67,8 +40,6 @@ enum Crpyto_SymCyp {
  * 	In the future (MAYBE), it may be possible to choose other hash algorithm 
  * 	such as PBKDF2 or Bcrypt
  *
- * 	It may also be possible to store in the DB the parameters of the 
- * 	hash.
  */
 int hash_password(const char * password, const char * salt, char * hash);
 
